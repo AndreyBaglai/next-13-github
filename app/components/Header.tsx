0f19fe0256fee28b2613d16ff8b5cd982713aecc
Andrey Baglay <andreybaglai@gmail.com>
2023-08-02 17:39:10 +0300

    add header

---
+++ app/components/Header.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="logo">
+          <Link href="/">Logo</Link>
+        </div>
+        <div className="links">
+          {" "}
+          <Link href="/about">About</Link>{" "}
+          <Link href="/about/team">About Team</Link>{" "}
+          <Link href="/code/repos">Code</Link>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
-import { Poppins } from "next/font/google";
-import "./globals.css";
+// types
 import type { Metadata } from "next";
+
+// components
+import Header from "./components/Header";
+
+// fonts
+import { Poppins } from "next/font/google";
+
+// styles
+import "./globals.css";
 
 const poppins = Poppins({
   weight: ["400", "700"],
@@ -20,7 +28,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <Header />
+        <main className="container">{children}</main>
+      </body>
     </html>
   );
 }
