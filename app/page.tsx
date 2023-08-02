f72b3ffa993baf30ee53c39aa81d4b12e72108c8
Andrey Baglay <andreybaglai@gmail.com>
2023-08-02 14:42:34 +0300

    add basic routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
+import Link from "next/link";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Some title</h1>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/about/team">Team</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
